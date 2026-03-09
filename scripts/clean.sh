1b1620e0d683ad5be29037ef82b55bed24776d1c
RyanDiaz <deus.ryan.diaz@gmail.com>
2026-03-09 20:06:06 +0100

    better project layout

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -8,8 +8,10 @@ find . -type d -name ".pytest_cache" -exec rm -rf {} +
 find . -type d -name ".mypy_cache" -exec rm -rf {} +
 find . -type d -name "*.debhelper" -exec rm -rf {} +
 find . -type d -name ".pybuild" -exec rm -rf {} +
+find . -type d -name ".build-venv" -exec rm -rf {} +
 find . -type d -name "build" -exec rm -rf {} +
 find . -type d -name "dist" -exec rm -rf {} +
+find . -type d -name "deb_dist" -exec rm -rf {} +
 find . -type d -name "*.egg-info" -exec rm -rf {} +
 
 echo "Done."
