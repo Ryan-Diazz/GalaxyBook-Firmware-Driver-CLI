a9df0eba6a70e56829ae70b9940dd04bb2fb74ea
RyanDiaz <deus.ryan.diaz@gmail.com>
2026-03-09 11:43:58 +0100

    try debian build

---
+++ clean.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Cleaning Python build artifacts..."
+
+find . -type d -name "__pycache__" -exec rm -rf {} +
+find . -type f \( -name "*.pyc" -o -name "*.pyo" \) -delete
+find . -type d -name ".pytest_cache" -exec rm -rf {} +
+find . -type d -name ".mypy_cache" -exec rm -rf {} +
+find . -type d -name "*.debhelper" -exec rm -rf {} +
+find . -type d -name ".pybuild" -exec rm -rf {} +
+find . -type d -name "build" -exec rm -rf {} +
+find . -type d -name "dist" -exec rm -rf {} +
+find . -type d -name "*.egg-info" -exec rm -rf {} +
+
+echo "Done."
