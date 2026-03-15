e0bf726ea080b447aea2adcd5381fc6b0a2e00ce
RyanDiaz <deus.ryan.diaz@gmail.com>
2026-03-15 11:51:51 +0100

    fix power lid 'function not found' and cleaner 'sudo required' print

--- src/galaxybook/utils/perm.py
+++ src/galaxybook/utils/perm.py
@@ -6,5 +6,4 @@ def require_root():
         cmd = os.path.basename(sys.argv[0])
         print("This command must be run as root!")
         print(f"Try: sudo {cmd} {' '.join(sys.argv[1:])}")
-        print(f"module ran from {sys.argv[0]}")
         sys.exit(1)
